739d6d9710545ef2dabd5c91295d6b76294d7df0
DsukeDyc <dsuke_dyc@yahoo.co.jp>
2021-04-13 15:34:39 +0900

    Isuue6

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -6,10 +6,6 @@ class CalendarsController < ApplicationController
     @plan = Plan.new
   end
 
-  #def new
-   # @plan = Plan.new
-  #end
-
   # 予定の保存
   def create
     Plan.create(plan_params)
@@ -24,11 +20,10 @@ class CalendarsController < ApplicationController
 
   def getWeek
     wdays = ['(日)','(月)','(火)','(水)','(木)','(金)','(土)']
-    #@wdays = Date.today.wday
 
     # Dateオブジェクトは、日付を保持しています。下記のように`.today.day`とすると、今日の日付を取得できます。
     @todays_date = Date.today
-    # 例)　今日が2月1日の場合・・・ Date.today.day => 1日
+    # 例) 今日が2月1日の場合・・・ Date.today.day => 1日
 
     @week_days = []
     @wdays = []
@@ -41,12 +36,19 @@ class CalendarsController < ApplicationController
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
 
-      #wday_num = Date.today.wday
-      #if wday_num >= 7
-        #wday_num = wday_num -7
+      wday_num = Date.today.wday[x + 1]#wdayメソッドを用いて取得した数値
+      if wday_num >= 7
+        wday_num = wday_num -7
+      end
+      
+      #wdays = Date.today.wday
+      #if wdays[x] = 0
+        #wdays[x] = wdays[x] + 1
+      #else wdays[x] >= 8
+        #wdays[x] = wdays[x] -7
       #end
 
-      days = { :month => (@todays_date + x).month, :date => (@todays_date+x).day, :plans => today_plans}
+      days = { :month => (@todays_date + x).month, :date => (@todays_date + x).day, :plans => today_plans, :wdays => wdays[wday_num] }
       @week_days.push(days)
     end
   end
